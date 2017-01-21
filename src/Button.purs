393a8da59931fbdb9cec3be32a6825f6f8349fae
kRITZCREEK <christoph.hegemann1337@gmail.com>
2017-01-21 12:30:39 +0100

    embed button actions in parent component

--- src/Button.purs
+++ src/Button.purs
@@ -7,32 +7,30 @@ import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Trans.Class (lift)
 
 import Thermite as T
-import React as R
 import React.DOM as R
 import React.DOM.Props as RP
 
 
 type ButtonState = Unit
 
+initButton :: Unit
 initButton = unit
 
-data ButtonAction parentAction
-  = Clicked parentAction
+data ButtonAction = Clicked
 
 
-buttonSpec :: forall eff props parentAction
-            . { _onClick :: parentAction
-              , _value   :: String
+buttonSpec :: forall eff props
+            . { _value   :: String
               }
-           -> T.Spec (console :: CONSOLE | eff) ButtonState props (ButtonAction parentAction)
+           -> T.Spec (console :: CONSOLE | eff) ButtonState props ButtonAction
 buttonSpec details = T.simpleSpec performAction render
   where
-    render :: T.Render ButtonState props (ButtonAction parentAction)
+    render :: T.Render ButtonState props ButtonAction
     render dispatch _ state _ =
-      [ R.button [ RP.onClick $ \_ -> dispatch $ Clicked details._onClick ]
+      [ R.button [ RP.onClick $ \_ -> dispatch Clicked ]
           [R.text details._value]
       ]
 
     performAction :: T.PerformAction (console :: CONSOLE | eff)
-                       ButtonState props (ButtonAction parentAction)
-    performAction (Clicked _) _ _ = lift $ liftEff $ log "button clicked!"
+                       ButtonState props ButtonAction
+    performAction Clicked _ _ = lift $ liftEff $ log "button clicked!"
--- src/Main.purs
+++ src/Main.purs
@@ -1,11 +1,11 @@
 module Main where
 
-import Button (ButtonState, initButton, ButtonAction, buttonSpec)
+import Button (ButtonState, initButton, ButtonAction(..), buttonSpec)
 import Thermite as T
 import React.DOM as R
 import DOM (DOM)
 
-import Data.Function (($))
+import Data.Function (($), const)
 import Data.Unit (Unit, unit)
 import Data.Semigroup ((<>))
 import Data.Ring ((-))
@@ -22,8 +22,6 @@ import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Trans.Class (lift)
 
-
-
 type CounterState =
   { _count :: Int
   , _incButton :: ButtonState
@@ -39,36 +37,29 @@ incButton = lens (\x -> x._incButton) (\x q -> x { _incButton = q })
 decButton :: Lens' CounterState ButtonState
 decButton = lens (\x -> x._decButton) (\x q -> x { _decButton = q })
 
-
 init :: CounterState
 init = { _count: 0, _incButton: initButton, _decButton: initButton }
 
 data CounterAction
   = Increment
   | Decrement
-  | IncButton (ButtonAction CounterAction)
-  | DecButton (ButtonAction CounterAction)
 
-_IncButton :: Prism' CounterAction (ButtonAction CounterAction)
-_IncButton = prism' IncButton $ \x -> case x of
-                                        IncButton y -> Just y
-                                        _           -> Nothing
+_IncButton :: Prism' CounterAction ButtonAction
+_IncButton = prism' (const Increment) $ case _ of
+  Increment -> Just Clicked
+  _ -> Nothing
 
-_DecButton :: Prism' CounterAction (ButtonAction CounterAction)
-_DecButton = prism' DecButton $ \x -> case x of
-                                        DecButton y -> Just y
-                                        _           -> Nothing
+_DecButton :: Prism' CounterAction ButtonAction
+_DecButton = prism' (const Decrement) $ case _ of
+  Decrement -> Just Clicked
+  _ -> Nothing
 
 counterSpec :: forall eff props
              . T.Spec (console :: CONSOLE | eff) CounterState props CounterAction
 counterSpec = T.simpleSpec performAction render
   where
-    inc = T.focus incButton _IncButton $ buttonSpec { _onClick: Increment
-                                                    , _value: "Increment"
-                                                    }
-    dec = T.focus decButton _DecButton $ buttonSpec { _onClick: Decrement
-                                                    , _value: "Decrement"
-                                                    }
+    inc = T.focus incButton _IncButton $ buttonSpec {_value: "Increment"}
+    dec = T.focus decButton _DecButton $ buttonSpec {_value: "Decrement"}
 
     render :: T.Render CounterState props CounterAction
     render dispatch props state children =
@@ -83,13 +74,6 @@ counterSpec = T.simpleSpec performAction render
     performAction Decrement _ state = do
       T.writeState $ over count (\x -> x-1) state
       lift $ liftEff $ log "decremented"
-    performAction action@(IncButton _) props state = do
-      lift $ liftEff $ log "inc button action being performed"
-      (inc ^. T._performAction) action props state
-    performAction action@(DecButton _) props state = do
-      lift $ liftEff $ log "dec button action being performed"
-      (dec ^. T._performAction) action props state
-
 
 main :: forall eff. Eff ( console :: CONSOLE
                         , dom :: DOM
